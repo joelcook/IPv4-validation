1b966a0218fd2d6ef33d39cd9a6479cdf78fbe93
joelcook <joeltcook93@gmail.com>
2017-02-01 12:51:16 -0500

    Add files via upload

---
+++ get-services.ps1
@@ -0,0 +1,28 @@
+#Joel Cook
+# PowerShell Sript to show running services
+#CNT 4603
+
+#param is for command line arg, auto default (*) value if no arg given
+param($nameReq = "*")
+
+#start of opening message
+""
+write-host "Script execution beings..." -ForegroundColor Yellow
+""
+write-host "Input parameter verified..." -ForegroundColor Green
+""
+write-host "The following" -ForegroundColor Red -NoNewline
+write-host " $nameReq " -ForegroundColor Green -NoNewline
+write-host "services are currently running on: " -ForegroundColor Red -NoNewline
+write-host "$env:computername" -ForegroundColor Magenta
+""
+#end of message
+
+#get-service layout
+Get-Service -Displayname $nameReq | Where-Object {$_.Status -eq "Running"} | select name, displayname, servicetype, status | sort state, name
+
+#ending message
+""
+write-host "List of Running Services Complete..." -ForegroundColor Blue -BackgroundColor Yellow
+""
+write-host "Scripting terminating..." -ForegroundColor Red -BackgroundColor Black
---
+++ ipaddress.py
@@ -0,0 +1,21 @@
+'''
+@author Joel Cook
+created 11/29/2016
+CNT 4603
+'''
+
+import sys
+import os
+import re
+
+fileName = input('Please enter the name of the file containing the input IPv4 addresses:')
+fileObject = open(fileName,"r")
+allLines = fileObject.readlines()
+fileObject.close()
+#valid ip address can only go to 255 so if 25 then the next character can also only be 0-5
+newRegex = re.compile("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+for eachLine in allLines:
+    if newRegex.match(eachLine):
+        print ("Valid IPv4 address found: ",eachLine)
+    else:
+        print("ERROR - invalid IPv4 address found: ",eachLine)
